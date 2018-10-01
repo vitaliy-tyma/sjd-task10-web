5835502b6d3c436a15305161b407e79dc05465f0
Vitaliy-tyma <vitaliy.tymashkov@gmail.com>
2018-10-01 14:28:30 +0300

    123456

--- src/info/sjd/AppRunner.java
+++ src/info/sjd/AppRunner.java
@@ -3,7 +3,7 @@ package info.sjd;
 public class AppRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 123
+		// TODO Auto-generated method stub 123456
 
 	}
 

34f5835b607768f93253f9fcff0ec7e64c2334bb
Vitaliy-tyma <vitaliy.tymashkov@gmail.com>
2018-10-03 15:49:30 +0300

    Tests are updated.

--- test/info/sjd/service/DataSaverTest.java
+++ test/info/sjd/service/DataSaverTest.java
@@ -13,7 +13,7 @@ class DataSaverTest {
 				" Intel Core i5 (8th Gen)", " 8GB RAM", " 256GB SSD",
 				" Back-lit Keyboard - Dark Silver – 15Z980-U.AAS5U1" };
 
-		// assertTrue(!DataSaver.parseName(product_title_quals).isEmpty());
+		assertTrue(!DataSaver.parseName(product_title_quals).isEmpty());
 		assertNotNull(DataSaver.parseName(product_title_quals));
 		assertEquals(DataSaver.parseName(product_title_quals), "LG gram Thin and Light Laptop");
 	}
